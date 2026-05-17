9e26d67b8c89ccefba9fab1bd5ae4194d9917341
LaibaJameel365 <laibajameel365@gmail.com>
2026-05-17 12:23:40 -0700

    Add files via upload

--- constants.py
+++ constants.py
@@ -1,4 +1,4 @@
-# Copyright 2022 The TensorFlow Authors. All Rights Reserved.
+# Copyright 2016 The TensorFlow Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,23 +12,124 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""Constants used in Trackable for checkpointing and serialization."""
+"""Constants for SavedModel save and restore operations.
 
-import enum
+The source of truth for these constants is in
+tensorflow/cc/saved_model/constants.h.
 
+"""
+from tensorflow.python.saved_model.pywrap_saved_model import constants
+from tensorflow.python.util.tf_export import tf_export
 
-# Key where the object graph proto is saved in a TensorBundle
-OBJECT_GRAPH_PROTO_KEY = "_CHECKPOINTABLE_OBJECT_GRAPH"
+# Subdirectory name containing the asset files.
+ASSETS_DIRECTORY = constants.ASSETS_DIRECTORY
+tf_export(
+    "saved_model.ASSETS_DIRECTORY",
+    v1=[
+        "saved_model.ASSETS_DIRECTORY", "saved_model.constants.ASSETS_DIRECTORY"
+    ]).export_constant(__name__, "ASSETS_DIRECTORY")
 
-# A key indicating a variable's value in an object's checkpointed Tensors
-# (Trackable._gather_saveables_for_checkpoint). If this is the only key and
-# the object has no dependencies, then its value may be restored on object
-# creation (avoiding double assignment when executing eagerly).
-VARIABLE_VALUE_KEY = "VARIABLE_VALUE"
-OBJECT_CONFIG_JSON_KEY = "OBJECT_CONFIG_JSON"
+# Subdirectory name containing unmanaged files from higher-level APIs.
+EXTRA_ASSETS_DIRECTORY = constants.EXTRA_ASSETS_DIRECTORY
 
+# CollectionDef key containing SavedModel assets.
+ASSETS_KEY = constants.ASSETS_KEY
+tf_export(
+    "saved_model.ASSETS_KEY",
+    v1=["saved_model.ASSETS_KEY",
+        "saved_model.constants.ASSETS_KEY"]).export_constant(
+            __name__, "ASSETS_KEY")
 
-@enum.unique
-class SaveType(str, enum.Enum):
-  SAVEDMODEL = "savedmodel"
-  CHECKPOINT = "checkpoint"
+# CollectionDef key for the legacy init op.
+LEGACY_INIT_OP_KEY = constants.LEGACY_INIT_OP_KEY
+tf_export(
+    v1=[
+        "saved_model.LEGACY_INIT_OP_KEY",
+        "saved_model.constants.LEGACY_INIT_OP_KEY"
+    ]).export_constant(__name__, "LEGACY_INIT_OP_KEY")
+
+# CollectionDef key for the SavedModel main op.
+MAIN_OP_KEY = constants.MAIN_OP_KEY
+tf_export(
+    v1=["saved_model.MAIN_OP_KEY",
+        "saved_model.constants.MAIN_OP_KEY"]).export_constant(
+            __name__, "MAIN_OP_KEY")
+
+# CollectionDef key for the SavedModel train op.
+# Not exported while export_all_saved_models is experimental.
+TRAIN_OP_KEY = constants.TRAIN_OP_KEY
+
+# Schema version for SavedModel.
+SAVED_MODEL_SCHEMA_VERSION = constants.SAVED_MODEL_SCHEMA_VERSION
+tf_export(
+    "saved_model.SAVED_MODEL_SCHEMA_VERSION",
+    v1=[
+        "saved_model.SAVED_MODEL_SCHEMA_VERSION",
+        "saved_model.constants.SAVED_MODEL_SCHEMA_VERSION"
+    ]).export_constant(__name__, "SAVED_MODEL_SCHEMA_VERSION")
+
+# File name prefix for SavedModel protocol buffer.
+SAVED_MODEL_FILENAME_PREFIX = constants.SAVED_MODEL_FILENAME_PREFIX
+
+# File name for SavedModel protocol buffer.
+SAVED_MODEL_FILENAME_PB = constants.SAVED_MODEL_FILENAME_PB
+tf_export(
+    "saved_model.SAVED_MODEL_FILENAME_PB",
+    v1=[
+        "saved_model.SAVED_MODEL_FILENAME_PB",
+        "saved_model.constants.SAVED_MODEL_FILENAME_PB"
+    ]).export_constant(__name__, "SAVED_MODEL_FILENAME_PB")
+
+# File name for SavedModel chunked protocol buffer (experimental).
+SAVED_MODEL_FILENAME_CPB = constants.SAVED_MODEL_FILENAME_CPB
+
+# File name for text version of SavedModel protocol buffer.
+SAVED_MODEL_FILENAME_PBTXT = constants.SAVED_MODEL_FILENAME_PBTXT
+tf_export(
+    "saved_model.SAVED_MODEL_FILENAME_PBTXT",
+    v1=[
+        "saved_model.SAVED_MODEL_FILENAME_PBTXT",
+        "saved_model.constants.SAVED_MODEL_FILENAME_PBTXT"
+    ]).export_constant(__name__, "SAVED_MODEL_FILENAME_PBTXT")
+
+# Subdirectory where debugging related files are written.
+DEBUG_DIRECTORY = constants.DEBUG_DIRECTORY
+tf_export(
+    "saved_model.DEBUG_DIRECTORY",
+    v1=[
+        "saved_model.DEBUG_DIRECTORY",
+        "saved_model.constants.DEBUG_DIRECTORY",
+    ]).export_constant(__name__, "DEBUG_DIRECTORY")
+
+# File name for GraphDebugInfo protocol buffer which corresponds to the
+# SavedModel.
+DEBUG_INFO_FILENAME_PB = constants.DEBUG_INFO_FILENAME_PB
+tf_export(
+    "saved_model.DEBUG_INFO_FILENAME_PB",
+    v1=[
+        "saved_model.DEBUG_INFO_FILENAME_PB",
+        "saved_model.constants.DEBUG_INFO_FILENAME_PB"
+    ]).export_constant(__name__, "DEBUG_INFO_FILENAME_PB")
+
+# Subdirectory name containing the variables/checkpoint files.
+VARIABLES_DIRECTORY = constants.VARIABLES_DIRECTORY
+tf_export(
+    "saved_model.VARIABLES_DIRECTORY",
+    v1=[
+        "saved_model.VARIABLES_DIRECTORY",
+        "saved_model.constants.VARIABLES_DIRECTORY"
+    ]).export_constant(__name__, "VARIABLES_DIRECTORY")
+
+# File name used for variables.
+VARIABLES_FILENAME = constants.VARIABLES_FILENAME
+tf_export(
+    "saved_model.VARIABLES_FILENAME",
+    v1=[
+        "saved_model.VARIABLES_FILENAME",
+        "saved_model.constants.VARIABLES_FILENAME"
+    ]).export_constant(__name__, "VARIABLES_FILENAME")
+
+# The initialization and train ops for a MetaGraph are stored in the
+# signature def map. The ops are added to the map with the following keys.
+INIT_OP_SIGNATURE_KEY = constants.INIT_OP_SIGNATURE_KEY
+TRAIN_OP_SIGNATURE_KEY = constants.TRAIN_OP_SIGNATURE_KEY
